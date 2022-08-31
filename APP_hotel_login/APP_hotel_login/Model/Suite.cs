d1cc407d13e8ea2116618722310c76b1f223d83a
fernandotrevisan <fernando.trevisan01@etec.sp.gov.br>
2022-08-31 15:45:56 -0300

    Adicionar arquivos de projeto.

---
+++ APP_hotel_login/APP_hotel_login/Model/Suite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace APP_hotel_login.Model
+{
+    public class Suite
+    {
+        public string Nome { get; set; }
+        public double DiariaAdulto { get; set; }
+        public double DiariaCrianca { get; set; }
+    }
+}
